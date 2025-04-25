82ce39bd1d87856be57be86a3690a2771a8e350c
Andy Huang <ahuang29@usc.edu>
2025-04-25 02:52:13 +0000

    hw5 done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool schedHelper(
+    AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    size_t maxShifts,
+    DailySchedule& sched,
+    vector<int> assigned,
+    size_t day,
+	size_t col
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +46,47 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    
+    sched.assign(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID)); //initialize sched with invalid id
+    vector<int> assigned(avail[0].size(), 0); //stores the number of shifts for each worker
+    AvailabilityMatrix temp = avail;
 
+    return schedHelper(temp, dailyNeed, maxShifts, sched, assigned, 0, 0);
+}
 
+bool schedHelper(
+    AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    size_t maxShifts,
+    DailySchedule& sched,
+    vector<int> assigned,
+    size_t day,
+	size_t col
+)
+{
+    //return true when all days have been scheduled
+	if(day == avail.size())
+		return true;
 
+    //loops throught each worker
+	for(Worker_T i = 0; i < avail[0].size(); ++i){
+		if(avail[day][i] == true && assigned[i] < maxShifts){ //runs if worker is available and does not exceed a certain number of shifts
+			sched[day][col] = i; //assigns worker
+			assigned[i]++; // adds 1 to the # of shifts that the worker now has
+            bool temp;
+			if(col + 1 == dailyNeed) 
+				temp = schedHelper(avail, dailyNeed, maxShifts, sched, assigned, day + 1, 0); //move on to next day of daily need is met
+			else
+				temp = schedHelper(avail, dailyNeed, maxShifts, sched, assigned, day, col + 1); //move on to next column (another worker) if daily need is not met
+            
+            if(temp)
+                return true;
+            //backtrack
+            assigned[i]--;
+            sched[day][col] = INVALID_ID;
+            
+		}
+	}
+	return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,12 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void generateGuesses(
+    string& in,
+    string& floating,
+    set<string>& dict,
+    set<string>& guesses,
+    int index);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +27,54 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    string input = in;
+    string yellow = floating;
+    set<string> dictionary = dict;
+    set<string> guesses;
+    generateGuesses(input, yellow, dictionary, guesses, 0);
+    return guesses;
 }
 
 // Define any helper functions here
+void generateGuesses(
+    string& in,
+    string& floating,
+    set<string>& dict,
+    set<string>& guesses,
+    int index)
+{
+    if(in.empty()) //if input is empty return empty set
+        return;
+
+    if(index == in.size()){ //if we reached the end position of the word
+        if(floating.empty() && dict.find(in) != dict.end()) // add word if all floating letters used & word is in dictionary
+            guesses.insert(in);
+      return;
+    }
+
+    if(in[index] != '-'){ //if letter is known at this position, move on to next position
+      generateGuesses(in, floating, dict, guesses, index + 1);
+      return;
+    }
+
+		//tries all possible letters in blank
+    for(char i='a'; i<='z'; ++i){
+        in[index] = i;
+        size_t pos = floating.find(i);
+        if(pos != string::npos){   		//checks if this is one of the floating letters
+            string newFloating = floating;
+            newFloating.erase(pos,1);
+            generateGuesses(in, newFloating, dict, guesses, index+ 1);
+        }
+        else{				// if not floating		
+            int count = 0;
+            for(int i = index + 1; i < in.size(); ++i){ //count number of blanks
+                if(in[i] == '-')
+                    count++;
+            }
+            if(floating.size() <= count + 1) //moves onto next index if unused floating letters can fit later
+                generateGuesses(in, floating, dict, guesses, index+1);
+        }
+    }
+	in[index] = '-'; //reset to blank so other iterations can try other letters
+}
